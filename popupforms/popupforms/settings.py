cf8d3baf02f993b1d8b17c3d20cbb4b5a52de049
edcodes <42159403+edcodes@users.noreply.github.com>
2021-12-02 13:41:27 -0800

    Update settings.py

--- popupforms/popupforms/settings.py
+++ popupforms/popupforms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(3=&7kod#klc66ieiupsak)5=x!iaps_*ynp%823j83za7cbe7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.176.136']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
